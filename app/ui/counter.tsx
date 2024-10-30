ebf6a38d7f53d3f4dab98d32f0407475ca397f9f
richard tsang <richard.tsang9641@gmail.com>
2024-10-30 18:27:52 +0800

    Add increment by amount, async, thunk

--- app/ui/counter.tsx
+++ app/ui/counter.tsx
@@ -1,16 +1,21 @@
 "use client";
-import { decrement, increment } from "@/lib/slice";
+import { decrement, increment, incrementAsync, incrementByAmount } from "@/lib/slice";
 import { RootState } from "@/lib/store";
-import { useSelector, useDispatch } from "react-redux";
+import { useAppDispatch, useAppSelector } from "@/lib/hooks";
+import { useState } from "react";
 export default function Counter() {
-  const count = useSelector((state: RootState) => state.counter.value);
-  const dispatch = useDispatch();
+  const count = useAppSelector((state: RootState) => state.counter.value);
+  const dispatch = useAppDispatch();
+
+  const [incrementAmount, setIncrementAmount] = useState('2');
+
+  const incrementValue = Number(incrementAmount) || 0
   return (
     <div>
       <h3>Counter (basic)</h3>
       <div className="flex gap-3">
         <button
-          className="relative p-3 bg-gray-100 rounded-full flex justi"
+          className="relative p-3 bg-gray-100 rounded-full hover:bg-gray-200 transition"
           aria-label="Decrement value"
           onClick={() => dispatch(decrement())}
         >
@@ -18,13 +23,18 @@ export default function Counter() {
         </button>
         <span className=" w-9 text-center">{count}</span>
         <button
-          className="relative p-3 bg-gray-100 rounded-full flex justi"
+          className="relative p-3 bg-gray-100 rounded-full hover:bg-gray-200 transition"
           aria-label="Increment value"
           onClick={() => dispatch(increment())}
         >
           <span className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-black font-bold">+</span>
 
         </button>
+      </div>
+      <div className="flex flex-col">
+        <input className=" text-black p-1 w-auto" aria-label="Set increment amount" value={incrementAmount} onChange={(e) => setIncrementAmount(e.target.value)} />
+        <button onClick={() => dispatch(incrementByAmount(incrementValue))}>increment by amount</button>
+        <button onClick={() => dispatch(incrementAsync(incrementValue))}>increment async</button>
       </div>
     </div>
   );
--- lib/store.ts
+++ lib/store.ts
@@ -1,4 +1,4 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { Action, configureStore, ThunkAction } from "@reduxjs/toolkit";
 import { counterSlice } from "./slice";
 
 export const makeStore = () => {
@@ -12,3 +12,4 @@ export type AppStore = ReturnType<typeof makeStore>;
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<AppStore["getState"]>;
 export type AppDispatch = AppStore["dispatch"];
+export type AppThunk<ThunkReturnType = void> = ThunkAction<ThunkReturnType, RootState, unknown, Action>
